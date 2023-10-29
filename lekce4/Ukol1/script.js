85b094384b3f0a9b0ab4ca4e12bd145f46920676
LiviaVCzechi <129322332+LiviaVCzechi@users.noreply.github.com>
2023-10-29 22:35:29 +0100

    Update script.js
    Signed-off-by: LiviaVCzechi <129322332+LiviaVCzechi@users.noreply.github.com>

--- lekce4/Ukol1/script.js
+++ lekce4/Ukol1/script.js
@@ -1,13 +1,3 @@
-//Úkol č. 1 FIT email
-/*const jmeno = prompt("Jmeno bez diakritiky").trim();
-const prijmeni = prompt("Prijmeni bez diakritiky").trim();
-const email = { 
-    jmenoKurz: jmeno.slice(0, 3),
-    prijmeniKurz: prijmeni.slice(0, 5),
-    };  
-document.body.innerHTML = "<p>Email: " + email.prijmeniKurz.toLowerCase() + email.jmenoKurz.toLowerCase() + "@fit.cvut.cz"
-"</p>" */
-
 //Úkol č. 2 Množstevní slevy
 const pocetKusu = prompt ("Zadej počet kusů: ")
 {
@@ -32,6 +22,3 @@ const pocetKusu = prompt ("Zadej počet kusů: ")
         document.body.innerHTML = "<p>Cena: " + pocetKusu * 120 + "</p>";
     }
   }
-
-
-
